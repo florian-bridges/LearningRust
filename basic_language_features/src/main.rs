5892e431310c172eb49bc2c76eca59ec8bbdbf67
florian <florianbridges97@gmail.com>
2024-08-15 21:47:13 +0200

    feat: control flows

--- basic_language_features/src/main.rs
+++ basic_language_features/src/main.rs
@@ -81,6 +81,80 @@ fn main() {
 
     let a = [3; 5]; // array of length 5 containing value 3
 
+    another_function(5);
+    print_labeled_measurement(5, 'h');
+
+    // statements and expressions
+
+    let y = {
+        let x = 3;
+        x + 1
+    };
+
+    println!("The value of y is: {y}");
+
+
+    //**************************************************************//
+    //                         Control Flow                         //
+    //**************************************************************//
+
+    let number = 3;
+
+    if number < 5 {
+        println!("condition was true");
+    } else {
+        println!("condition was false");
+    }
+
+    let condition = true;
+    let number = if condition { 5 } else { 6 };
+    println!("The value of number is: {number}");
+
+    let mut counter = 0;
+
+    let result = loop {
+        counter += 1;
+        println!("The value of counter is: {counter}");
+        if counter == 10 {
+            break counter * 2;
+        }
+    };
+    println!("The final value of counter is: {counter} result is {result}");
+
+
+    // loop labels
+    let mut count = 0;
+    'counting_up: loop {
+        println!("count = {count}");
+        let mut remaining = 10;
+
+        loop {
+            println!("remaining = {remaining}");
+            if remaining == 9 {
+                break;
+            }
+            if count == 2 {
+                break 'counting_up;
+            }
+            remaining -= 1;
+        }
+
+        count += 1;
+    }
+    println!("End count = {count}");
+
+    let a = [10, 20, 30, 40, 50];
+
+    for element in a {
+        println!("the value is: {element}");
+    }
 
 }
 
+fn another_function(x: i32) {
+    println!("The value of x is: {x}");
+}
+
+fn print_labeled_measurement(value: i32, unit_label: char) {
+    println!("The measurement is: {value}{unit_label}");
+}
