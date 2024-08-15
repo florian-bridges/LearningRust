e50db6e5d369791f774e4a3c8a898f4f8e1be066
Florian Bridges <florianbridges97@gmail.com>
2024-08-15 14:41:11 +0200

    feat: basic variables

---
+++ basic_language_features/src/main.rs
@@ -0,0 +1,23 @@
+// This is a constant:
+const THREE_HOURS_IN_SECONDS: u32 = 60 * 60 * 3;
+
+fn main() {
+    let mut x = 5;
+    println!("The value of x is: {x}");
+    x = 6;
+    println!("The value of x is: {x}");
+    println!("Three hours in seconds: {THREE_HOURS_IN_SECONDS}");
+
+    // Shadowing:
+
+    let x = x + 1;
+
+    {
+        let x = x * 2;
+        println!("The value of x in the inner scope is: {x}");
+        // The value of x in the inner scope is: 14
+    }
+
+    println!("The value of x is: {x}");
+    // The value of x is: 7
+}
